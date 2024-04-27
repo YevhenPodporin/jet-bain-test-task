689c1c889fa622fe134691b1ba18d2f48226f8d5
yevhen.podporin <yevhen.podporin@nure.ua>
2024-04-27 12:02:42 +0300

    Test task

---
+++ index.js
@@ -0,0 +1,38 @@
+const length = 10000000;
+const array = Array(length).fill().map((_, i) => i + 1);
+
+array.splice(-1,1);
+array.splice(-1,1);
+
+// Сложность алгоритма O(n)
+const findAbsentNumbers = (array, length) => {
+	const absentNumbers = [];
+
+	// Проверяем начало массива, если array не начинается с 1, добавляем недостающие числа
+	let current = 1;
+	for (const num of array) {
+		// Если текущий ожидаемый номер меньше числа в массиве, значит число отсутствует
+		while (current < num) {
+			absentNumbers.push(current);
+			current += 1;
+			if (absentNumbers.length === 2) {
+				return absentNumbers;
+			}
+		}
+		// устанавливаем следующее ожидаемое число как текущее для следующей итерации
+		current = num + 1;
+	}
+
+	// Если текущий номер не достиг `length`, добавляем оставшиеся недостающие числа
+	while (current <= length && absentNumbers.length < 2) {
+		absentNumbers.push(current);
+		current += 1;
+	}
+
+	return absentNumbers;
+};
+
+console.time('O(n)');
+const result1 = findAbsentNumbers(array, length)
+console.timeEnd('O(n)');
+console.log(result1)
